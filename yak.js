2ccd8d04c7a1fdcc2a94c57ec814848a64646e38
glathoud <glathoud@yahoo.fr>
2014-12-18 16:33:10 +0100

    escape

--- yak.js
+++ yak.js
@@ -41,6 +41,8 @@
                 .replace( /&/g, '&amp;' )            
                 .replace( /</g, '&lt;' )
                 .replace( />/g, '&gt;' )            
+                .replace( /"/g, '&quot;' )
+                .replace( /'/g, '&apos;' )
             ;
         }
 
